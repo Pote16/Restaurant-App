5c213f875abe77e54f5760946c7bace868504508
Dominik Pototschnig <dpotot@gmail.com>
2022-01-04 22:12:59 +0100

    updated endpoints

--- nodeAPI/src/restaurantAPI.ts
+++ nodeAPI/src/restaurantAPI.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { Order, OrderedItem, IOrderDB } from "./database";
-import { IOrderAPI, IMenuCategoryAPI, IMenuItemAPI, IOrderedItemAPI, IUserAPI } from "./interfaces"
+import { Order, OrderedItem, IOrderDB, OrderStatus } from "./database";
+import { IOrderAPI, IMenuCategoryAPI, IMenuItemAPI, IOrderedItemAPI, IUserAPI, } from "./interfaces"
 import { dblogger } from "./Logger";
 
 const logger = dblogger;
@@ -54,16 +54,18 @@ export async function getOrders(req: Request, res: Response) {
 }
 
 export async function deleteOrders(req: Request, res: Response) {
-
+    OrderStatus.destroy({
+        where: {},
+        truncate: true
+    });
 }
 
 export async function postOrders(req: Request, res: Response) {
     try {
         let order = req.body as IOrderAPI;
 
-        let neworder = await Order.create({
-            orderID: 1,
-            orderStatusID: order.status,
+        const neworder = await Order.create({
+            orderStatusID: req.body.status,
             orderDate: order.orderDate | Date.now(),
             tableID: order.tableId,
             paymentReference: order.paymentReference,
@@ -71,16 +73,25 @@ export async function postOrders(req: Request, res: Response) {
             totalAmount: order.totalAmount
         });
 
-        for (let item of order.orderedItems) {
-            await neworder.createOrderedItem({
-                orderID: neworder._attributes.orderID,
-                itemID: item.itemId,
-                number: item.number,
-                orderItemSatusID: item.status
-            });
-        }
+        await neworder.createOrderedItems({
+            orderID: 1,
+            itemID: 6,
+            number: 1,
+            orderItemSatusID: 1
+        });
 
-        res.status(200).send("created new Order");
+        /*         
+        for (let item of order.orderedItems) {
+                    await neworder.createOrderedItems({
+                        orderID: neworder.orderID,
+                        itemID: item.itemId,
+                        number: item.number,
+                        orderItemSatusID: item.status
+                    });
+                }
+        */
+
+        res.status(200).json(neworder);
 
     } catch (error) {
         logger.error(error);
@@ -127,11 +138,37 @@ export async function getOrderByID(req: Request, res: Response) {
 }
 
 export async function putOrderByID(req: Request, res: Response) {
-    let id = req.params.id;
+    //let id = req.params.id;
+    try {
+        let newOrder = req.body; //as IOrderAPI;
+        let order = await Order.findByPk(req.params.id);
+
+        if (order) {
+            order.orderStatusID = newOrder.status ? newOrder.status : order.orderStatusID;
+            order.orderDate = newOrder.orderDate ? newOrder.orderDate : order.orderDate;
+            order.tableID = newOrder.tableId ? newOrder.tableId : order.tableID;
+            order.paymentReference = newOrder.paymentReference ? newOrder.paymentReference : order.paymentReference;
+            order.paymentToken = newOrder.paymentToken ? newOrder.paymentToken : order.paymentToken;
+            order.totalAmount = newOrder.totalAmount ? newOrder.totalAmount : order.totalAmount;
+        }
+        await order?.save();
+        res.status(200).json(order);
+    } catch (error) {
+        logger.error(error);
+        res.status(400).send("failed");
+    }
 }
 
 export async function deleteOrderByID(req: Request, res: Response) {
-    let id = req.params.id;
+    try {
+        let order = await Order.findByPk(req.params.id);
+        if (order) {
+            order.destroy();
+        }
+    } catch (error) {
+        logger.error(error);
+        res.status(400).send("failed");
+    }
 }
 
 
