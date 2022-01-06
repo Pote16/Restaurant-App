c361c17d35f1a8bc84ef1324599fe2a9608d8d5d
Dominik Pototschnig <dpotot@gmail.com>
2022-01-06 12:42:00 +0100

    added routes

--- nodeAPI/src/router.ts
+++ nodeAPI/src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import * as orderHandler from "./handlers/restaurantAPI";
+import * as orderHandler from "./handlers/orderAPI";
 import * as menuItemHandler from "./handlers/menuItemAPI";
+import * as tableHandler from "./handlers/tableAPI";
 export const router = Router();
 
 
@@ -8,10 +9,12 @@ export const router = Router();
 router.get('/orders', orderHandler.getOrders);
 router.post('/orders', orderHandler.postOrders); //post a new order
 router.delete('/orders', orderHandler.deleteOrders); //delte all orders
+router.get('/orders/status', orderHandler.getOrderStatusList);
 
 router.get('/orders/:id', orderHandler.getOrderByID);
 router.put('/orders/:id', orderHandler.putOrderByID); //update single Order
 router.delete('/orders/:id', orderHandler.deleteOrderByID);
+
 
 //TODO: Users API
 
@@ -23,6 +26,10 @@ router.get('/menuItems', menuItemHandler.getMenuItems);
 
 
 //TODO: TableAPI  :Dominik
+
+router.get('/tableItems', tableHandler.getTables);
+router.post('/tableItems', tableHandler.postTable);
+
 //TODO: Statuse Get und Post  :Dominik
 
 
