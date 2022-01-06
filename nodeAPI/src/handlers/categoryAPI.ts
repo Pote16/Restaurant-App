a32c5ecf7868985ef5a5f49c739a4d40be5a7d3d
Philipp Fischbacher <ph.fischbacher@gmail.com>
2022-01-06 15:19:49 +0100

    add POST and PUT menuCategory calls

--- nodeAPI/src/handlers/categoryAPI.ts
+++ nodeAPI/src/handlers/categoryAPI.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { dblogger } from "../Logger";
-import {MenuCategory} from "../database";
+import {ITableDB, MenuCategory, Table} from "../database";
 import {IMenuCategoryAPI} from "../interfaces";
 
 const logger = dblogger;
@@ -18,6 +18,53 @@ export async function getMenuCategories(req: Request, res: Response) {
       retArray.push(ret);
     }
     res.status(200).json(retArray);
+  } catch (error) {
+    logger.error(error);
+    res.status(400).send("failed");
+  }
+}
+
+export async function getMenuCategoryByID(req: Request, res: Response) {
+  try {
+    let menuCategory = await MenuCategory.findByPk(req.params.id);
+    if(menuCategory) {
+      let ret : IMenuCategoryAPI = {
+        categoryId: menuCategory.categoryID,
+        title: menuCategory.title,
+        desc: menuCategory.desc
+      }
+      res.status(200).json(ret);
+    }
+  } catch (error) {
+    logger.error(error);
+    res.status(400).send("failed");
+  }
+}
+
+export async function postMenuCategory(req: Request, res: Response) {
+  try {
+    let menuCategory = req.body as IMenuCategoryAPI;
+    let newMenuCategory = await MenuCategory.create({
+      title: menuCategory.title,
+      desc: menuCategory.desc
+    });
+    res.status(200).json(newMenuCategory);
+  } catch (error) {
+    logger.error(error);
+    res.status(400).send("failed");
+  }
+}
+
+export async function putMenuCategoryByID(req: Request, res: Response) {
+  try {
+    let newMenuCategory = req.body as IMenuCategoryAPI;
+    let menuCategory = await MenuCategory.findByPk(req.params.id);
+
+    if (menuCategory) {
+      menuCategory.title = newMenuCategory.title ? newMenuCategory.title : menuCategory.title;
+      menuCategory.desc = newMenuCategory.desc ? newMenuCategory.desc : menuCategory.desc;
+    }
+    res.status(200).json(await menuCategory?.save());
   } catch (error) {
     logger.error(error);
     res.status(400).send("failed");
--- nodeAPI/src/router.ts
+++ nodeAPI/src/router.ts
@@ -3,7 +3,7 @@ import * as orderHandler from "./handlers/orderAPI";
 import * as menuItemHandler from "./handlers/menuItemAPI";
 import * as tableHandler from "./handlers/tableAPI";
 import * as categoryHandler from "./handlers/categoryAPI";
-import {getMenuCategories} from "./handlers/categoryAPI";
+import {getMenuCategories, getMenuCategoryByID, postMenuCategory, putMenuCategoryByID} from "./handlers/categoryAPI";
 export const router = Router();
 
 
@@ -20,9 +20,15 @@ router.delete('/orders/:id', orderHandler.deleteOrderByID);
 
 //TODO: Users API
 
-//TODO: CategoryAPI
+//Category API
 router.get('/categories', categoryHandler.getMenuCategories);
+router.post('/categories', categoryHandler.postMenuCategory);
 
+router.get('/categories/:id', categoryHandler.getMenuCategoryByID);
+router.put('/categories/:id', categoryHandler.putMenuCategoryByID);
+
+
+//MenuItems API
 router.get('/menuItems', menuItemHandler.getMenuItems);
 router.delete('/menuItems', menuItemHandler.deleteMenuItems);
 router.post('/menuItems', menuItemHandler.postMenuItem);
@@ -30,10 +36,8 @@ router.post('/menuItems', menuItemHandler.postMenuItem);
 router.get('/menuItems/:id', menuItemHandler.getMenuItemByID);
 router.delete('/menuItems/:id', menuItemHandler.deleteMenuItemByID);
 router.put('/menuItems/:id', menuItemHandler.putMenuItemByID);
-//TODO: TableAPI
 
-
-//TODO: TableAPI  :Dominik
+//Table API
 router.get('/tables', tableHandler.getTables);
 router.post('/tables', tableHandler.postTable);
 router.get('/tables/:id', tableHandler.getTableByID);
