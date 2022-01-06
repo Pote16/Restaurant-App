009e762ecc4b835026d6707b9e3a515bc457b37d
Philipp Fischbacher <ph.fischbacher@gmail.com>
2022-01-06 16:33:56 +0100

    add authenticator middleware

--- nodeAPI/src/auth/authAPI.ts
+++ nodeAPI/src/auth/authAPI.ts
@@ -1,24 +1,22 @@
-import {Request, Response} from "express";
 import { User} from "../database";
 import { dblogger } from "../Logger";
 
 const logger = dblogger;
 
-export async function getUser(req: Request, res: Response) {
+export async function getUser(username: string, password: string) {
   try {
     let user = await User.findOne({
       where: {
-        name: req.body.name,
-        password: req.body.password
+        name: username,
+        password: password
       }
     });
     if (user) {
-      res.status(200).json(user);
+      return user;
     } else {
-      res.status(400).send("No User with this combination");
+      return "No User with this combination";
     }
   } catch (error) {
     logger.error(error);
-    res.status(400).send("failed");
   }
 }
--- nodeAPI/src/auth/authenticator.ts
+++ nodeAPI/src/auth/authenticator.ts
@@ -1,1 +1,41 @@
+import {Request, Response} from "express";
+import * as userHandler from "./authAPI";
+import {User} from "../database";
+
 const jwt = require('jsonwebtoken');
+
+export async function login(req: Request, res: Response) {
+  let username = req.body.user;
+  let password = req.body.pass;
+  let user = userHandler.getUser(username, password);
+  if(user instanceof Object) {
+    const token = jwt.sign(
+      {
+        username: username,
+        password: password,
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn: process.env.EXPIRATION
+      });
+    res.status(200).json({
+      message: "login successful",
+      login: username,
+      token: token
+    });
+  } else {
+    res.status(403).json({
+      message: "Not logged in. Wrong username/password combination!"
+    });
+  }
+}
+
+export function isAuthorized(req: Request, res: Response, next: Function) {
+  try {
+    const jwtToken = req.headers.authorization;
+    jwt.verify(jwtToken, process.env.JWT_KEY);
+    next();
+  } catch (e) {
+    return res.status(401).json({message: "Authentication failed"});
+  }
+}
