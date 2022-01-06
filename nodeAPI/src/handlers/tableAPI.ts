86ef4a7204799ab9534f13743aa36adf29b03250
Dominik Pototschnig <dpotot@gmail.com>
2022-01-06 13:27:23 +0100

    added handlers

--- nodeAPI/src/handlers/tableAPI.ts
+++ nodeAPI/src/handlers/tableAPI.ts
@@ -5,7 +5,7 @@ import { dblogger } from "../Logger";
 const logger = dblogger;
 
 
-//@TODO Handle Orders
+//@TODO Handle tables
 export async function getTables(req: Request, res: Response) {
     try {
 
@@ -29,6 +29,50 @@ export async function postTable(req: Request, res: Response) {
         });
         res.status(200).json(newtable);
 
+    } catch (error) {
+        logger.error(error);
+        res.status(400).send("failed");
+    }
+}
+
+export async function getTableByID(req: Request, res: Response) {
+    try {
+        let table = await Table.findByPk(req.params.id);
+        if (table) {
+            res.status(200).json(table);
+        } else {
+            res.status(400).send("No Table with this ID");
+        }
+    } catch (error) {
+        logger.error(error);
+        res.status(400).send("failed");
+    }
+}
+
+export async function putTableByID(req: Request, res: Response) {
+    try {
+        let newTable: ITableDB = req.body; //as ItableAPI;
+        let table = await Table.findByPk(req.params.id);
+
+        if (table) {
+            table.anzahlPlatz = newTable.anzahlPlatz ? newTable.anzahlPlatz : table.anzahlPlatz;
+            table.beschreibung = newTable.beschreibung ? newTable.beschreibung : table.beschreibung;
+        }
+        res.status(200).json(await table?.save());
+    } catch (error) {
+        logger.error(error);
+        res.status(400).send("failed");
+    }
+}
+
+
+export async function deleteTableByID(req: Request, res: Response) {
+    try {
+        let table = await Table.findByPk(req.params.id);
+        if (table) {
+            table.destroy();
+        }
+        res.status(200).send("deleted table");
     } catch (error) {
         logger.error(error);
         res.status(400).send("failed");
