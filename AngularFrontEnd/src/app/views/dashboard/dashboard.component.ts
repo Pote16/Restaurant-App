1d21f4776564f27db8e03f252ab9d32ed5b1127c
Dominik Pototschnig <dpotot@gmail.com>
2022-02-03 15:42:55 +0100

    deleted old imports

--- AngularFrontEnd/src/app/views/dashboard/dashboard.component.ts
+++ AngularFrontEnd/src/app/views/dashboard/dashboard.component.ts
@@ -3,19 +3,6 @@ import { FormControl, FormGroup } from '@angular/forms';
 
 import { DashboardChartsData, IChartProps } from './dashboard-charts-data';
 
-interface IUser {
-  name: string;
-  state: string;
-  registered: string;
-  country: string;
-  usage: number;
-  period: string;
-  payment: string;
-  activity: string;
-  avatar: string;
-  status: string;
-  color: string;
-}
 
 @Component({
   templateUrl: 'dashboard.component.html',
