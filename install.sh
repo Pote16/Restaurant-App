7323f1bd0b7972f45f4ede20efa1dcce2844392c
Philipp Fischbacher <ph.fischbacher@gmail.com>
2022-02-13 13:26:51 +0100

    start scripts

---
+++ install.sh
@@ -0,0 +1,8 @@
+cd nodeAPI/
+npm install
+
+tput setaf 2; echo "NodeAPI modules successfully installed"
+
+cd ../AngularFrontend
+npm install
+tput setaf 2; echo "AngularFrontend modules successfully installed"
